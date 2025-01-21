cca87b04f994ce1f066229a7f225f6f5a26c3a8d
Will Adams <bushblade@gmail.com>
2025-01-21 15:28:11 +0000

    remove unused models from Schema

--- db/seed.tsx
+++ db/seed.tsx
@@ -6,9 +6,6 @@ async function main() {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
   await prisma.user.deleteMany();
-  await prisma.account.deleteMany();
-  await prisma.session.deleteMany();
-  await prisma.verificationToken.deleteMany();
   await prisma.product.createMany({ data: sampleData.products });
   const users = [];
   for (let i = 0; i < sampleData.users.length; i++) {
